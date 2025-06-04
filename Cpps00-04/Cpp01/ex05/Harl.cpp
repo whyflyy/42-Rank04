8402e6fb4187c20ddb1098a3d920fb36f338d236
Whyflyy <cavadasribeiro.joao@gmail.com>
2025-06-04 17:24:05 +0100

    Started ex05 @

---
+++ Cpps00-04/Cpp01/ex05/Harl.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 16:35:31 by jcavadas          #+#    #+#             */
+/*   Updated: 2025/06/04 16:59:23 by jcavadas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+//DEBUG - BLUE/MAGENTA "Watch where the birds fly, they will lead us to land."
+//INFO - GREEN/BLUE "Six hundred men, six hundred miles of open sea."
+//WARNING - YELLOW "This land confuses your mind."
+//ERROR - RED "All I hear are screams, every time I dare to close my eyes."
