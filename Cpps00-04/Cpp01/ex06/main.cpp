d620c994a8dafffa28ff00a32c804067635f10dc
Whyflyy <cavadasribeiro.joao@gmail.com>
2025-06-11 12:36:23 +0100

    Did a minor change to accept complaint without being in caps

--- Cpps00-04/Cpp01/ex06/main.cpp
+++ Cpps00-04/Cpp01/ex06/main.cpp
@@ -6,14 +6,28 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 17:18:49 by jcavadas          #+#    #+#             */
-/*   Updated: 2025/06/09 17:22:42 by jcavadas         ###   ########.fr       */
+/*   Updated: 2025/06/11 12:10:29 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+std::string ToUpper(std::string str)
+{
+    std::string newstr = str;
+
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        newstr[i] = std::toupper(str[i]);
+    }
+
+    return newstr;
+}
+
 int main(int argc, char **argv)
 {
+	std::string	complaint;
+
 	switch (argc)
 	{
 		case 1:
@@ -21,7 +35,8 @@ int main(int argc, char **argv)
 			return (1);
 		case 2:
 			Harl Chato;
-			Chato.complain(argv[1]);
+			complaint = ToUpper(argv[1]);
+			Chato.complain(complaint);
 			return (0);
 	}
 	std::cout << "You complain too much!" << std::endl;
